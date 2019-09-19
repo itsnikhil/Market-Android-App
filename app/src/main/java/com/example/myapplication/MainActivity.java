c35d99cddaf48d0b89b8fe6e48726b7a0c09b4c7
itsnikhil <taneja.nikhil03@gmail.com>
2019-09-20 02:06:28 +0530

    bug fix spree

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -75,6 +75,10 @@ public class MainActivity extends AppCompatActivity
                     .placeholder(R.drawable.placeholder)
                     .into(userPhoto);
         }
+        else{
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -100,41 +104,6 @@ public class MainActivity extends AppCompatActivity
             //use the query to search your data somehow
             Toast.makeText(getApplicationContext(), "You searched for: "+query, Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
     @Override
